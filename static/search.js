f0b47577d5f9e5b6f01251b37a1b1ec75b54c3e5
Barney Govan <barney@boomtrain.com>
2016-05-26 18:44:49 -0700

    Resize map

--- static/search.js
+++ static/search.js
@@ -10,6 +10,7 @@ $("#search_btn").on('click', function(evt){
     $.get("/search.json", {"term": searchTerm}, displayResults)
 });
 console.log('loaded');
+
 function displayResults(data) {
     // adding the info to the page here?
     var dataDestination = $("#search-results");
@@ -17,11 +18,19 @@ function displayResults(data) {
     // debugger;
     var markers = new L.MarkerClusterGroup();
 
+
+    var bounds = map.getBounds();
+
     for (var i = 0; i < data.data.length; i++) {
-        
+
         var each = data.data[i];
         var title = each.name;
         var latlong = [Number(each.lat), Number(each.lon)];
+
+        bounds._southWest._lat = Math.min(latlong[0], bounds._southWest._lat);
+        bounds._southWest._lng = Math.min(latlong[0], bounds._southWest._lng);
+        bounds._northEast._lat = Math.min(latlong[0], bounds._northEast._lat);
+        bounds._northEast._lng = Math.min(latlong[0], bounds._northEast._lng);
         var link = each.route+each.id;
         dataDestination.append('<li><a href="'+link+'">'+each.name+'</a></li><br>'+each.lat);
 
@@ -29,16 +38,14 @@ function displayResults(data) {
             icon: L.mapbox.marker.icon({'marker-symbol': 'rocket', 'marker-color': '0044FF'}),
             title: title
         });
-        
+
         marker.bindPopup(title);
         markers.addLayer(marker);
 
     }
 
     map.addLayer(markers);
-    
 
-    
-
+    map.fitBounds(bounds);
 }
 
